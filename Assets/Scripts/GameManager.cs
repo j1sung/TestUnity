8ebd67f9fc7a598498e0c3de32b35e5878ac5c95
j1sung <dalssagi00@gmail.com>
2024-11-03 17:53:42 +0900

    Update 멀티플레이 구현 완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
 {
+    public GameObject playerPrefab;
+    
+    private int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 randomSpawnPos = Random.insideUnitSphere*3f;
+        randomSpawnPos.y = 0f;
+        PhotonNetwork.Instantiate(playerPrefab.name, randomSpawnPos, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(score);
+        }
+        else
+        {
+            score = (int)stream.ReceiveNext();
+        }
     }
 }
